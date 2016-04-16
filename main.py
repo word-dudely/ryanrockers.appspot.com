18959e6f8b2afc002f85f8a670660210a2f83f49
Ryan Rockers <ryanrockers@gmail.com>
2016-04-16 12:56:42 -0700

    Big improvement with Random Redditbot. Using the google fonts and <hr>s

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ randomRedditbot_html="""
 <title>Random Redditbot</title>
 <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
 <link type="text/css" rel="stylesheet" href="/stylesheets/randomRedditbot.css" />
-
+<link href='https://fonts.googleapis.com/css?family=Anton&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
@@ -137,33 +137,33 @@ randomRedditbot_html="""
 
 <script>
 $(document).ready(function() {
-var displayText = "<p>";
-var randomSortList = ['hot', 'rising', 'controversial', 'top', 'new'];
-var randomSubredditList = ['all', 'breakingmom', 'dadjokes', 'news', 'space', 'economy', 'business', 'onthescene', 'science', 'Advice', 'confessions', 'riddles', 'UFOs', 'myfriendwantstoknow', 'howto', 'dogs', 'conspiracy', 'oldpeoplefacebook', 'tifu', 'artificial', 'Seattle', 'tech', 'technews', 'democrats', 'republicans', 'inthenews', 'finance', 'gossip', 'singularity', 'AskTechnology', 'AMA', 'paranormal', 'mildlyinteresting', 'worldnews', 'nottheonion', 'shittyaskscience', 'explainlikeimfive', 'philosophy', 'askphilosophy', 'LearnUselessTalents', 'TechNewsToday', 'AmericanPolitics', 'Economics', 'mildlyinfuriating', 'creepyPMs', 'Whatisthis', 'Libertarian', 'MURICA', 'FutureWhatIf', 'wikipedia', 'sanders4president', 'the_donald', 'Hillary_Clinton', 'TedCruzForPresident', 'KasichForPresident', 'cringe', 'cringepics', 'niceguys'];
-var usedList = [['[deleted]',0], [undefined,0], ['[removed]',0]];
-var intervalID = window.setInterval(randomReddit, 4000);
+    var displayText = "<p>";
+    var randomSortList = ['hot', 'rising', 'controversial', 'top', 'new'];
+    var randomSubredditList = ['all', 'breakingmom', 'dadjokes', 'news', 'space', 'economy', 'business', 'onthescene', 'science', 'Advice', 'confessions', 'riddles', 'UFOs', 'myfriendwantstoknow', 'howto', 'dogs', 'conspiracy', 'oldpeoplefacebook', 'tifu', 'artificial', 'Seattle', 'tech', 'technews', 'democrats', 'republicans', 'inthenews', 'finance', 'gossip', 'singularity', 'AskTechnology', 'AMA', 'paranormal', 'mildlyinteresting', 'worldnews', 'nottheonion', 'shittyaskscience', 'explainlikeimfive', 'philosophy', 'askphilosophy', 'LearnUselessTalents', 'TechNewsToday', 'AmericanPolitics', 'Economics', 'mildlyinfuriating', 'creepyPMs', 'Whatisthis', 'Libertarian', 'MURICA', 'FutureWhatIf', 'wikipedia', 'sanders4president', 'the_donald', 'Hillary_Clinton', 'TedCruzForPresident', 'KasichForPresident', 'cringe', 'cringepics', 'niceguys'];
+    var usedList = [['[deleted]',0], [undefined,0], ['[removed]',0]];
+    var intervalID = window.setInterval(randomReddit, 4000);
 
-function randomReddit() {
-$.getJSON('https://www.reddit.com/r/' + randomSubredditList[Math.floor(Math.random() * randomSubredditList.length)] + '/' + randomSortList[Math.floor(Math.random() * randomSortList.length)] + '/.json?limit=1&t=hour', function(data) {
-var link='https://www.reddit.com/r/all/comments/' + (data.data.children[0].data.id);
-$.getJSON('https://www.reddit.com/r/all/comments/' + (data.data.children[0].data.id) + '.json?limit=1', function(data) {
-var comment=data[1].data.children[0].data.body;
-for (var i=0;i<usedList.length;i++) {
-if (usedList[i].indexOf(comment) != -1) {
-return;
-}
-}
-usedList.push([comment,link]);
-displayText="<p>";
-for (var i=usedList.length-1;i>2;i--) {
-displayText += '<a href="'+ usedList[i][1] + '" target="_blank">' + usedList[i][0] + "</a><p>";
-}
-$("#results").html(displayText);
-//window.scrollTo(0,document.body.scrollHeight)
-});
-});
-};
-randomReddit();
+    function randomReddit() {
+        $.getJSON('https://www.reddit.com/r/' + randomSubredditList[Math.floor(Math.random() * randomSubredditList.length)] + '/' + randomSortList[Math.floor(Math.random() * randomSortList.length)] + '/.json?limit=1&t=hour', function(data) {
+        var link='https://www.reddit.com/r/all/comments/' + (data.data.children[0].data.id);
+        $.getJSON('https://www.reddit.com/r/all/comments/' + (data.data.children[0].data.id) + '.json?limit=1', function(data) {
+            var comment=data[1].data.children[0].data.body;
+            for (var i=0;i<usedList.length;i++) {
+            if (usedList[i].indexOf(comment) != -1) {
+            return;
+            }
+            }
+            usedList.push([comment,link]);
+            displayText="<p>";
+            for (var i=usedList.length-1;i>2;i--) {
+            displayText += '<a href="'+ usedList[i][1] + '" target="_blank">' + usedList[i][0] + "</a><p><hr><p>";
+            }
+            $("#results").html(displayText);
+            //window.scrollTo(0,document.body.scrollHeight)
+            });
+        });
+    };
+    randomReddit();
 });
 </script>
 
